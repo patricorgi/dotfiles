800352cb16140ad568534c80a33a8b29e157ea51
Hangyi Wu <hangyi.wu@cern.ch>
2024-08-29 15:03:09 +0200

    refactor: remove a few plugins

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -65,4 +65,24 @@ return {
     },
   },
   { 'patricorgi/vim-snakemake', ft = 'Snakefile' },
+  {
+    'ray-x/lsp_signature.nvim',
+    event = 'VeryLazy',
+    main = 'lsp_signature',
+    opts = {
+      hint_enable = false, -- disable hints as it will crash in some terminal
+    },
+    specs = {
+      {
+        'folke/noice.nvim',
+        optional = true,
+        opts = {
+          lsp = {
+            signature = { enabled = false },
+            hover = { enabled = false },
+          },
+        },
+      },
+    },
+  },
 }
