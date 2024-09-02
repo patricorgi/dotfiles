93f01db1942b49ebdee1ef7a27ed4369334103d9
Hangyi Wu <patricorgi@gmail.com>
2024-09-02 15:37:28 -0400

    fix: root_dir under single file mode for basedpyright

--- .config/nvim/lua/custom/config/lsp.lua
+++ .config/nvim/lua/custom/config/lsp.lua
@@ -207,6 +207,28 @@ require('mason-lspconfig').setup {
 local local_servers = {
   basedpyright = {
     cmd = { 'basedpyright-langserver', '--stdio' },
+    settings = {
+      basedpyright = {
+        analysis = { typeCheckingMode = 'off' },
+      },
+    },
+    root_dir = function(fname)
+      local util = require 'lspconfig.util'
+      local dir_name = util.root_pattern(unpack {
+        'pyproject.toml',
+        'setup.py',
+        'setup.cfg',
+        'requirements.txt',
+        'Pipfile',
+        'pyrightconfig.json',
+        '.git',
+      })(fname)
+      if dir_name == nil then
+        return vim.fs.dirname(fname)
+      else
+        return dir_name
+      end
+    end,
   },
 }
 
