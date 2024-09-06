efaae510443d544ab37ab8960846eebb796dcd54
Hangyi Wu <patricorgi@gmail.com>
2024-09-05 23:18:43 -0400

    feat,refactor: cbfmt formatter for code blocks in md

---
+++ .config/nvim/lua/custom/config/conform.lua
@@ -0,0 +1,47 @@
+require('conform').setup {
+  notify_on_error = false,
+  format_on_save = function(bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    -- Disable "format_on_save lsp_fallback" for languages that don't
+    -- have a well standardized coding style. You can add additional
+    -- languages here or re-enable it for the disabled ones.
+    local disable_filetypes = { c = true }
+    return {
+      timeout_ms = 2000,
+      lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+    }
+  end,
+  formatters_by_ft = {
+    lua = { 'stylua' },
+    cpp = { 'clang-format' },
+    python = { 'yapf', 'isort' },
+    sh = { 'shfmt' },
+    snakemake = { 'snakefmt' },
+    markdown = { 'prettierd', 'cbfmt' },
+  },
+  formatters = {
+    cbfmt = { command = 'cbfmt', args = { '-w', '--config', vim.fn.expand '~' .. '/.config/cbfmt.toml', '$FILENAME' } },
+    yapf = { command = 'yapf' },
+  },
+}
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -31,8 +31,8 @@ return {
   { 'Bilal2453/luvit-meta', lazy = true },
   { -- Autoformat
     'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
+    event = { 'BufWritePre', "InsertEnter" },
+    cmd = { 'ConformInfo', "FormatEnable", "FormatDisable" },
     keys = {
       {
         '<leader>lf',
@@ -43,26 +43,9 @@ return {
         desc = 'Lsp Format buffer',
       },
     },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true }
-        return {
-          timeout_ms = 2000,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        cpp = { 'clang-format' },
-        python = { 'yapf', 'isort' },
-        sh = { 'shfmt' },
-        snakemake = { 'snakefmt' },
-      },
-    },
+    config = function()
+      require 'custom.config.conform'
+    end,
   },
   { 'patricorgi/vim-snakemake', ft = 'Snakefile' },
 }
