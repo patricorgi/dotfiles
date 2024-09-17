c25210899257fc45871e7b4369893e4a3f84e458
patricorgi <patricorgi@gmail.com>
2024-09-17 15:02:01 -0400

    perf: make nvim lazier and faster
    perf: make nvim lazier and faster
    
    perf: make nvim faster by removing mason-tool-installer.nvim

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -1,3 +1,10 @@
+local pip_args
+
+if vim.startswith(vim.fn.hostname(), 'n819') then
+  pip_args = { '--proxy', 'http://lbproxy:8080' }
+else
+  pip_args = {}
+end
 return {
   {
     -- Main LSP Configuration
@@ -5,11 +12,28 @@ return {
     event = { 'BufReadPost', 'BufNewFile' },
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
-      { 'williamboman/mason.nvim', event = 'VeryLazy', config = true }, -- NOTE: Must be loaded before dependants
+      {
+        'williamboman/mason.nvim',
+        event = 'VeryLazy',
+        opts = {
+          pip = {
+            ---@since 1.0.0
+            -- Whether to upgrade pip to the latest version in the virtual environment before installing packages.
+            upgrade_pip = false,
+
+            ---@since 1.0.0
+            -- These args will be added to `pip install` calls. Note that setting extra args might impact intended behavior
+            -- and is not recommended.
+            --
+            -- Example: { "--proxy", "https://proxyserver" }
+            install_args = pip_args,
+          },
+        },
+      }, -- NOTE: Must be loaded before dependants
       'williamboman/mason-lspconfig.nvim',
-      { 'WhoIsSethDaniel/mason-tool-installer.nvim', event = 'VeryLazy' },
+      -- { 'WhoIsSethDaniel/mason-tool-installer.nvim', event = 'VeryLazy' },
       -- Allows extra capabilities provided by nvim-cmp
-      { 'hrsh7th/cmp-nvim-lsp', event = 'VeryLazy' },
+      { 'hrsh7th/cmp-nvim-lsp', event = 'InsertEnter' },
     },
     config = function()
       require 'custom.config.lsp'
