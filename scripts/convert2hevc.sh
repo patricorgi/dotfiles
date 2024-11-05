724d364d9e45263f83d99df4ea75a8a93a56a226
patricorgi <patricorgi@gmail.com>
2024-11-05 08:20:56 -0500

    feat!: add ghostty config (pre-release)

--- scripts/convert2hevc.sh
+++ scripts/convert2hevc.sh
@@ -11,11 +11,11 @@ infile="${1}"
 original_profile=$(ffprobe -v error -select_streams v:0 -show_entries stream=profile -of default=noprint_wrappers=1:nokey=1 "$infile")
 original_codec=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "$infile")
 original_comment=$(exiftool -Comment -s3 "$infile")
-if [ $original_comment == *"BetterThanHEVC"* ]; then
+if [ "$original_comment" == *"BetterThanHEVC"* ]; then
 	osascript -e 'display notification "Already better than HEVC. Nothing to do." with title "HEVC converter"'
 	exit 1
 fi
-if [ $original_codec == "hevc" ] && [ $original_profile == "Main" ]; then
+if [ "$original_codec" == "hevc" ] && [ $original_profile == "Main" ]; then
 	osascript -e 'display notification "Already in HEVC. Nothing to do." with title "HEVC converter"'
 	exit 1
 fi
@@ -29,7 +29,7 @@ infile_size=$(wc -c $infile | cut -d' ' -f2)
 outfile_size=$(wc -c $outfile | cut -d' ' -f2)
 ratio=$(echo "$outfile_size*100 / $infile_size" | bc)
 isSmaller=$(echo "$ratio < 100" | bc)
-if [ $isSmaller -eq 1 ] || [ $original_profile != "Main" ]; then
+if [ "$isSmaller" -eq 1 ] || [ "$original_profile" != "Main" ]; then
 	trash "${infile}"
 	mv "${outfile}" "${infile}"
 	osascript -e 'display notification "Trash original file." with title "HEVC converter"'
