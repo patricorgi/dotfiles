ccb6f06e3d8f830855415b35c3d0b5c1ebbded89
patricorgi <patricorgi@gmail.com>
2024-12-30 14:26:12 -0500

    feat: adapt to yazi v0.4.2 api

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -5,7 +5,7 @@ require("full-border"):setup({
 
 function Linemode:size_and_mtime()
 	local year = os.date("%Y")
-	local time = (self._file.cha.modified or 0) // 1
+	local time = (self._file.cha.mtime or 0) // 1
 
 	if time > 0 and os.date("%Y", time) == year then
 		time = os.date("%b %d %H:%M", time)
