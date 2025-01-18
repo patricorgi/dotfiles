64793de96d4461ef8f0c5a712deeb5834da66251
patricorgi <patricorgi@gmail.com>
2025-01-18 00:45:56 -0500

    chore: cleanup lsp

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -1,37 +1,34 @@
 local pip_args
-
 if vim.startswith(vim.fn.hostname(), 'n819') then
   pip_args = { '--proxy', 'http://lbproxy:8080' } -- need LbEnv
 else
   pip_args = {}
 end
+
 return {
   {
     'neovim/nvim-lspconfig',
     event = { 'BufReadPost', 'BufNewFile' },
     cmd = { 'LspInfo', 'LspInstall', 'LspUninstall' },
-    dependencies = { { 'rmagatti/goto-preview', keys = { 'gp', 'gP' }, opts = {} } },
+    dependencies = {
+      {
+        'williamboman/mason.nvim',
+        opts = {
+          pip = {
+            upgrade_pip = false,
+            install_args = pip_args,
+          },
+          ui = {
+            border = 'single',
+            width = 0.7,
+            height = 0.7,
+          },
+        },
+      },
+      { 'rmagatti/goto-preview', keys = { 'gp', 'gP' }, opts = {} },
+    },
     config = function()
       require 'custom.config.lsp'
     end,
   },
-  {
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre', 'InsertEnter' },
-    cmd = { 'ConformInfo', 'FormatEnable', 'FormatDisable' },
-    keys = {
-      {
-        '<leader>lf',
-        function()
-          require('conform').format { async = true, lsp_fallback = true }
-        end,
-        mode = '',
-        desc = 'Lsp Format buffer',
-      },
-    },
-    config = function()
-      require 'custom.config.conform'
-    end,
-  },
-  { 'patricorgi/vim-snakemake', ft = { 'Snakefile', 'snakemake' } },
 }
