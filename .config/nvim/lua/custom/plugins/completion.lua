13dc8a93714232893b3b3f73c433c4caf18aec9b
patricorgi <patricorgi@gmail.com>
2025-01-21 21:00:30 -0500

    deprecate: LuaSnip

--- .config/nvim/lua/custom/plugins/completion.lua
+++ .config/nvim/lua/custom/plugins/completion.lua
@@ -1,17 +1,6 @@
 return {
   'saghen/blink.cmp',
   build = 'cargo build --release',
-  dependencies = {
-    'L3MON4D3/LuaSnip',
-    version = 'v2.*',
-    event = 'VeryLazy',
-    config = function()
-      require('luasnip.loaders.from_lua').load {
-        paths = { '~/.config/nvim/snippets' },
-      }
-      require('luasnip.loaders.from_vscode').lazy_load()
-    end,
-  },
   config = function()
     require 'custom.config.completion'
   end,
