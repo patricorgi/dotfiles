dd9a7f2c57829707101e9e0ea763e656416e009a
patricorgi <patricorgi@gmail.com>
2025-01-24 00:44:18 -0500

    feat: add colorful-menu

--- .config/nvim/lua/custom/plugins/completion.lua
+++ .config/nvim/lua/custom/plugins/completion.lua
@@ -1,6 +1,26 @@
 return {
   'saghen/blink.cmp',
-  build = 'cargo build --release',
+  event = { 'BufReadPost', 'BufNewFile' },
+  version = '1.*',
+  dependencies = {
+    'xzbdmw/colorful-menu.nvim',
+    config = function()
+      require('colorful-menu').setup {
+        ls = {
+          lua_ls = { arguments_hl = '@comment' },
+          clangd = {
+            extra_info_hl = '@comment',
+          },
+          basedpyright = {
+            extra_info_hl = '@comment',
+          },
+          fallback = true,
+        },
+        fallback_highlight = '@variable',
+        max_width = 60,
+      }
+    end,
+  },
   config = function()
     require 'custom.config.completion'
   end,
