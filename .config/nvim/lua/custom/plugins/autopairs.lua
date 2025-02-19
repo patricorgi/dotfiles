17184ed441f401411ea0f850eca2c2332a8ab097
patricorgi <patricorgi@gmail.com>
2025-02-18 23:59:50 -0500

    feat: add markdown plugins
    feat: math $ autopair

--- .config/nvim/lua/custom/plugins/autopairs.lua
+++ .config/nvim/lua/custom/plugins/autopairs.lua
@@ -11,6 +11,38 @@ return {
         cmap = '<C-q>',
         hopout = true,
       },
+      internal_pairs = {
+        { '$', '$', suround = true, fly = true, dosuround = true, newline = true, space = true, ft = { 'markdown', 'latex', 'tex' } },
+        { '[', ']', fly = true, dosuround = true, newline = true, space = true },
+        { '(', ')', fly = true, dosuround = true, newline = true, space = true },
+        { '{', '}', fly = true, dosuround = true, newline = true, space = true },
+        { '"', '"', suround = true, multiline = false },
+        {
+          "'",
+          "'",
+          suround = true,
+          cond = function(fn)
+            return not fn.in_lisp() or fn.in_string()
+          end,
+          alpha = true,
+          nft = { 'tex' },
+          multiline = false,
+        },
+        {
+          '`',
+          '`',
+          cond = function(fn)
+            return not fn.in_lisp() or fn.in_string()
+          end,
+          nft = { 'tex' },
+          multiline = false,
+        },
+        { '``', "''", ft = { 'tex' } },
+        { '```', '```', newline = true, ft = { 'markdown' } },
+        { '<!--', '-->', ft = { 'markdown', 'html' }, space = true },
+        { '"""', '"""', newline = true, ft = { 'python' } },
+        { "'''", "'''", newline = true, ft = { 'python' } },
+      },
     },
   },
 }
---
+++ .config/nvim/lua/custom/plugins/markdown.lua
@@ -0,0 +1,4 @@
+return {
+  'bullets-vim/bullets.vim',
+  ft = { 'markdown' },
+}
