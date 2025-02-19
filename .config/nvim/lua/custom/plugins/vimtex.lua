9c886dec5947b8993d355b2165ce9cfcbd81a6ce
patricorgi <patricorgi@gmail.com>
2025-02-18 23:54:14 -0500

    feat: add vimtex

---
+++ .config/nvim/lua/custom/plugins/vimtex.lua
@@ -0,0 +1,54 @@
+return {
+  'lervag/vimtex',
+  ft = { 'tex' },
+  lazy = false,
+  init = function()
+    vim.g.vimtex_view_method = 'sioyek'
+    vim.g.vimtex_context_pdf_viewer = 'sioyek'
+    vim.g.vimtex_view_sioyek_options = "--execute-command 'toggle_synctex;fit_to_page_width' --nofocus "
+    vim.g.vimtex_log_ignore = {
+      'Underfull',
+      'Overfull',
+      'specifier changed to',
+      'Token not allowed in a PDF string',
+    }
+    vim.g.vimtex_quickfix_ignore_filters = {
+      'Underfull',
+      'Overfull',
+      'specifier changed to',
+      'Token not allowed in a PDF string',
+      'LaTeX Warning: Float too large for page',
+      'contains only floats',
+    }
+    vim.g.vimtex_compiler_latexmk = {
+      build_dir = '.out',
+      options = {
+        '-shell-escape',
+        '-verbose',
+        '-file-line-error',
+        '-interaction=nonstopmode',
+        '-synctex=1',
+      },
+    }
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'VimtexEventViewReverse',
+      callback = function()
+        vim.system { 'open', '/Applications/kitty.app' }
+      end,
+    })
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'VimtexEventQuit',
+      callback = function()
+        vim.system { 'open', '/Applications/kitty.app' }
+      end,
+    })
+    vim.api.nvim_create_autocmd('VimEnter', {
+      callback = function()
+        local buf_ft = vim.bo.filetype
+        if buf_ft == 'tex' then
+          vim.cmd 'VimtexCompile'
+        end
+      end,
+    })
+  end,
+}
