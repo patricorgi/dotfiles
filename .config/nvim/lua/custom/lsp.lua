43847c0411015aeddd06551f7a38f4f0795a2f82
patricorgi <patricorgi@gmail.com>
2025-04-21 20:46:12 -0400

    feat!: nvim 0.11 ʕ•̫͡•ʕ•̫͡•ʔ•̫͡•ʔ
    Revert "feat: tiny-inline-diagnostic.nvim"
    
    This reverts commit b488130fdf24bbc35de2b920bd283d34d39f1ade.

--- .config/nvim/lua/custom/lsp.lua
+++ .config/nvim/lua/custom/lsp.lua
@@ -6,6 +6,7 @@ vim.lsp.enable 'ruff'
 vim.lsp.enable 'marksman'
 vim.lsp.enable 'tinymist'
 vim.lsp.enable 'texlab'
+vim.lsp.enable 'bashls'
 
 -- Define LSP-related keymaps
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -98,19 +99,19 @@ vim.api.nvim_create_autocmd('LspAttach', {
 -- diagnostic UI touches
 local icons = require 'custom.ui.icons'
 vim.diagnostic.config {
-  virtual_lines = { current_line = true },
-  -- virtual_text = {
-  --   spacing = 4,
-  --   prefix = '',
-  -- },
+  -- virtual_lines = { current_line = true },
+  virtual_text = {
+    spacing = 5,
+    prefix = '◍ ',
+  },
   float = { severity_sort = true },
   severity_sort = true,
   signs = {
     text = {
-      [vim.diagnostic.severity.ERROR] = icons.diagnostics.Error,
-      [vim.diagnostic.severity.WARN] = icons.diagnostics.Warn,
-      [vim.diagnostic.severity.INFO] = icons.diagnostics.Info,
-      [vim.diagnostic.severity.HINT] = icons.diagnostics.Hint,
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.INFO] = '',
+      [vim.diagnostic.severity.HINT] = '',
     },
   },
 }
