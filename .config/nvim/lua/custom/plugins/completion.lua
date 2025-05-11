87f311c344e16b63d6e98e57e8ce7604456b27e8
patricorgi <patricorgi@gmail.com>
2025-05-11 09:35:31 -0400

    chore: simplify blink.cmp config

--- .config/nvim/lua/custom/plugins/completion.lua
+++ .config/nvim/lua/custom/plugins/completion.lua
@@ -2,26 +2,46 @@ return {
   'saghen/blink.cmp',
   event = { 'BufReadPost', 'BufNewFile' },
   version = '1.*',
-  dependencies = {
-    'xzbdmw/colorful-menu.nvim',
-    config = function()
-      require('colorful-menu').setup {
-        ls = {
-          lua_ls = { arguments_hl = '@comment' },
-          clangd = {
-            extra_info_hl = '@comment',
+  dependencies = { 'xzbdmw/colorful-menu.nvim', opts = {} },
+  opts = {
+    completion = {
+      documentation = {
+        auto_show = true,
+      },
+      menu = {
+        draw = {
+          columns = { { 'kind_icon' }, { 'label', gap = 1 } },
+          components = {
+            label = {
+              text = function(ctx)
+                return require('colorful-menu').blink_components_text(ctx)
+              end,
+              highlight = function(ctx)
+                return require('colorful-menu').blink_components_highlight(ctx)
+              end,
+            },
           },
-          basedpyright = {
-            extra_info_hl = '@comment',
-          },
-          fallback = true,
         },
-        fallback_highlight = '@variable',
-        max_width = 60,
-      }
-    end,
+      },
+    },
+    keymap = {
+      ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+    },
+    signature = {
+      enabled = true,
+    },
+    cmdline = {
+      completion = {
+        menu = {
+          auto_show = true,
+        },
+      },
+    },
+    sources = {
+      providers = {
+        snippets = { score_offset = 1000 },
+      },
+    },
   },
-  config = function()
-    require 'custom.config.completion'
-  end,
 }
