950c351c86d906b7bafdb8d74a083e69de659b05
patricorgi <patricorgi@gmail.com>
2025-05-23 21:57:08 -0400

    remove: nvim-ufo (again!)

--- .config/nvim/lua/custom/config/folding.lua
+++ .config/nvim/lua/custom/config/folding.lua
@@ -1,52 +1,95 @@
-vim.o.foldcolumn = '1' -- '0' is not bad
+-- vim.o.foldcolumn = '1' -- '0' is not bad
+-- vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+-- vim.o.foldlevelstart = 99
+-- vim.o.foldenable = true
+-- vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+--
+-- local handler = function(virtText, lnum, endLnum, width, truncate)
+--   local newVirtText = {}
+--   local suffix = (' 🡗 %d '):format(endLnum - lnum)
+--   local sufWidth = vim.fn.strdisplaywidth(suffix)
+--   local targetWidth = width - sufWidth
+--   local curWidth = 0
+--   for _, chunk in ipairs(virtText) do
+--     local chunkText = chunk[1]
+--     local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+--     if targetWidth > curWidth + chunkWidth then
+--       table.insert(newVirtText, chunk)
+--     else
+--       chunkText = truncate(chunkText, targetWidth - curWidth)
+--       local hlGroup = chunk[2]
+--       table.insert(newVirtText, { chunkText, hlGroup })
+--       chunkWidth = vim.fn.strdisplaywidth(chunkText)
+--       -- str width returned from truncate() may less than 2nd argument, need padding
+--       if curWidth + chunkWidth < targetWidth then
+--         suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+--       end
+--       break
+--     end
+--     curWidth = curWidth + chunkWidth
+--   end
+--   table.insert(newVirtText, { suffix, 'MoreMsg' })
+--   return newVirtText
+-- end
+--
+-- -- global handler
+-- -- `handler` is the 2nd parameter of `setFoldVirtTextHandler`,
+-- -- check out `./lua/ufo.lua` and search `setFoldVirtTextHandler` for detail.
+-- ---@diagnostic disable-next-line: missing-fields
+-- -- require('ufo').setup {
+-- --   fold_virt_text_handler = handler,
+-- -- }
+-- require('ufo').setup {
+--   provider_selector = function(bufnr, filetype, buftype)
+--     return { 'treesitter', 'indent' }
+--   end,
+--   fold_virt_text_handler = handler,
+-- }
+--
+-- -- buffer scope handler
+-- -- will override global handler if it is existed
+-- -- local bufnr = vim.api.nvim_get_current_buf()
+-- -- require('ufo').setFoldVirtTextHandler(bufnr, handler)
+vim.o.foldcolumn = '0' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+vim.o.foldmethod = 'expr'
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 
-local handler = function(virtText, lnum, endLnum, width, truncate)
-  local newVirtText = {}
-  local suffix = (' 🡗 %d '):format(endLnum - lnum)
-  local sufWidth = vim.fn.strdisplaywidth(suffix)
-  local targetWidth = width - sufWidth
-  local curWidth = 0
-  for _, chunk in ipairs(virtText) do
-    local chunkText = chunk[1]
-    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-    if targetWidth > curWidth + chunkWidth then
-      table.insert(newVirtText, chunk)
-    else
-      chunkText = truncate(chunkText, targetWidth - curWidth)
-      local hlGroup = chunk[2]
-      table.insert(newVirtText, { chunkText, hlGroup })
-      chunkWidth = vim.fn.strdisplaywidth(chunkText)
-      -- str width returned from truncate() may less than 2nd argument, need padding
-      if curWidth + chunkWidth < targetWidth then
-        suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-      end
-      break
-    end
-    curWidth = curWidth + chunkWidth
+-- Source: https://www.reddit.com/r/neovim/comments/1fzn1zt/custom_fold_text_function_with_treesitter_syntax/
+local function fold_virt_text(result, s, lnum, coloff)
+  if not coloff then
+    coloff = 0
   end
-  table.insert(newVirtText, { suffix, 'MoreMsg' })
-  return newVirtText
+  local text = ''
+  local hl
+  for i = 1, #s do
+    local char = s:sub(i, i)
+    local hls = vim.treesitter.get_captures_at_pos(0, lnum, coloff + i - 1)
+    local _hl = hls[#hls]
+    if _hl then
+      local new_hl = '@' .. _hl.capture
+      if new_hl ~= hl then
+        table.insert(result, { text, hl })
+        text = ''
+        hl = nil
+      end
+      text = text .. char
+      hl = new_hl
+    else
+      text = text .. char
+    end
+  end
+  table.insert(result, { text, hl })
 end
-
--- global handler
--- `handler` is the 2nd parameter of `setFoldVirtTextHandler`,
--- check out `./lua/ufo.lua` and search `setFoldVirtTextHandler` for detail.
----@diagnostic disable-next-line: missing-fields
--- require('ufo').setup {
---   fold_virt_text_handler = handler,
--- }
-require('ufo').setup {
-  provider_selector = function(bufnr, filetype, buftype)
-    return { 'treesitter', 'indent' }
-  end,
-  fold_virt_text_handler = handler,
-}
-
--- buffer scope handler
--- will override global handler if it is existed
--- local bufnr = vim.api.nvim_get_current_buf()
--- require('ufo').setFoldVirtTextHandler(bufnr, handler)
+function _G.custom_foldtext()
+  local start = vim.fn.getline(vim.v.foldstart):gsub('\t', string.rep(' ', vim.o.tabstop))
+  local nline = vim.v.foldend - vim.v.foldstart
+  local result = {}
+  fold_virt_text(result, start, vim.v.foldstart - 1)
+  table.insert(result, { ' ... ↙ ' .. nline .. ' lines', 'DapBreakpointCondition' })
+  return result
+end
+vim.opt.foldtext = 'v:lua.custom_foldtext()'
--- .config/nvim/lua/custom/lsp.lua
+++ .config/nvim/lua/custom/lsp.lua
@@ -57,9 +57,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local client = vim.lsp.get_client_by_id(event.data.client_id)
     if client and client.supports_method 'textDocument/foldingRange' then
       local win = vim.api.nvim_get_current_win()
-      vim.wo[win][0].foldmethod = 'expr'
       vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
-      -- vim.notify 'Enable folding with LSP'
     end
 
     -- Inlay hint
