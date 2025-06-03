a27b3fa5d7aff1d7999b67e4554f4a983a21693b
patricorgi <patricorgi@gmail.com>
2025-06-03 09:27:24 -0400

    revert: remove colorful-menu

--- .config/nvim/lua/custom/plugins/completion.lua
+++ .config/nvim/lua/custom/plugins/completion.lua
@@ -2,26 +2,10 @@ return {
   'saghen/blink.cmp',
   event = { 'BufReadPost', 'BufNewFile' },
   version = '1.*',
-  dependencies = { 'xzbdmw/colorful-menu.nvim', opts = {} },
   opts = {
     completion = {
       documentation = {
         auto_show = true,
-      },
-      menu = {
-        draw = {
-          columns = { { 'kind_icon' }, { 'label', gap = 1 } },
-          components = {
-            label = {
-              text = function(ctx)
-                return require('colorful-menu').blink_components_text(ctx)
-              end,
-              highlight = function(ctx)
-                return require('colorful-menu').blink_components_highlight(ctx)
-              end,
-            },
-          },
-        },
       },
     },
     keymap = {
