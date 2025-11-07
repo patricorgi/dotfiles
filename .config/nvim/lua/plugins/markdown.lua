052aa9dd46d1fdd8eab285ab0e53087beda08864
patricorgi <patricorgi@gmail.com>
2025-11-06 20:48:23 -0500

    feat: changes in kitty

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,6 +1,6 @@
 vim.pack.add({
 	{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
-	{ src = "https://github.com/bullets-vim/bullets.vim" },
+	-- { src = "https://github.com/bullets-vim/bullets.vim" },
 })
 vim.lsp.enable("marksman")
 
