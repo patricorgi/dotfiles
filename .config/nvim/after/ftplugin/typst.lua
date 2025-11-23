0796f6a45f1f185c23c10907fb02465645b28e22
patricorgi <patricorgi@gmail.com>
2025-11-23 14:32:07 -0500

    feat: add typst-preview

--- .config/nvim/after/ftplugin/typst.lua
+++ .config/nvim/after/ftplugin/typst.lua
@@ -1,7 +1,8 @@
 if vim.b.did_my_ftplugin then
-  return
+	return
 end
 vim.b.did_my_ftplugin = true
+
 vim.lsp.enable("tinymist")
 vim.lsp.enable("typstyle")
 
@@ -16,20 +17,24 @@ end
 
 local typst_job_id = nil
 local function start_typst_watch()
-  local src = vim.fn.expand("%:p")
-  local pdf = vim.fn.expand("%:p:r") .. ".pdf"
+	local src = vim.fn.expand("%:p")
+	local pdf = vim.fn.expand("%:p:r") .. ".pdf"
 
-  -- Start typst watch only if not already running
-  if not typst_job_id or vim.fn.jobwait({ typst_job_id }, 0)[1] ~= -1 then
-    typst_job_id = vim.fn.jobstart(
-      { "typst", "watch", "--diagnostic-format", "short", src },
-      { stdout_buffered = true, stderr_buffered = true } -- no detach = killed with nvim
-    )
-  end
+	-- Start typst watch only if not already running
+	if not typst_job_id or vim.fn.jobwait({ typst_job_id }, 0)[1] ~= -1 then
+		typst_job_id = vim.fn.jobstart(
+			{ "typst", "watch", "--diagnostic-format", "short", src },
+			{ stdout_buffered = true, stderr_buffered = true } -- no detach = killed with nvim
+		)
+	end
 
-  -- (Re)open zathura every time
-  vim.fn.jobstart({ "zathura", pdf })
+	-- (Re)open zathura every time
+	vim.fn.jobstart({ "zathura", pdf })
 end
 
-vim.keymap.set("n", "<leader>tw", start_typst_watch,
-  { buffer = true, desc = "Start typst watch and open PDF in zathura" })
+vim.keymap.set(
+	"n",
+	"<leader>tw",
+	start_typst_watch,
+	{ buffer = true, desc = "Start typst watch and open PDF in zathura" }
+)
