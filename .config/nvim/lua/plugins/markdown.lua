4b4f0865aa64230471bab008896d1c9e5c747b67
patricorgi <patricorgi@gmail.com>
2026-01-09 13:09:24 -0500

    feat: lsp enable in lsp.lua

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -2,6 +2,5 @@ vim.pack.add({
 	{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
 	-- { src = "https://github.com/bullets-vim/bullets.vim" },
 })
-vim.lsp.enable("marksman")
 
 
