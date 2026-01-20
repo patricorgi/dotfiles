f55d4835724986f4c46ad677f8359af76b59aa1f
patricorgi <patricorgi@gmail.com>
2026-01-20 13:31:11 -0500

    feat: change keymaps for vim-tmux-navigator

--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -2,13 +2,13 @@ if vim.env.TMUX ~= nil and vim.env.TMUX ~= "" then
 	vim.pack.add({
 		"https://github.com/christoomey/vim-tmux-navigator",
 	})
-	vim.keymap.set("n", "<C-h>", "<cmd><C-U>TmuxNavigateLeft<cr>")
-	vim.keymap.set("n", "<C-j>", "<cmd><C-U>TmuxNavigateDown<cr>")
-	vim.keymap.set("n", "<C-k>", "<cmd><C-U>TmuxNavigateUp<cr>")
-	vim.keymap.set("n", "<C-l>", "<cmd><C-U>TmuxNavigateRight<cr>")
+	vim.keymap.set("n", "<M-h>", "<cmd>TmuxNavigateLeft<cr>")
+	vim.keymap.set("n", "<M-j>", "<cmd>TmuxNavigateDown<cr>")
+	vim.keymap.set("n", "<M-k>", "<cmd>TmuxNavigateUp<cr>")
+	vim.keymap.set("n", "<M-l>", "<cmd>TmuxNavigateRight<cr>")
 else
-	vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
-	vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
-	vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
-	vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+	vim.keymap.set("n", "<M-h>", "<C-w>h", { desc = "Go to left window" })
+	vim.keymap.set("n", "<M-j>", "<C-w>j", { desc = "Go to lower window" })
+	vim.keymap.set("n", "<M-k>", "<C-w>k", { desc = "Go to upper window" })
+	vim.keymap.set("n", "<M-l>", "<C-w>l", { desc = "Go to right window" })
 end
